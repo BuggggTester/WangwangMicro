bb442071cee0d207e2be21ff32444917732cf679
ArTist <1308666843@qq.com>
2024-08-23 10:52:59 +0800

    Create a new project

---
+++ src/main/java/com/example/wangwangmicro/WangwangMicroApplication.java
@@ -0,0 +1,13 @@
+package com.example.wangwangmicro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WangwangMicroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WangwangMicroApplication.class, args);
+	}
+
+}
