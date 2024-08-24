0b783dc461f8b9984891920f099658bd3985ab2b
xsrkt26 <xsrkt26@outlook.com>
2024-08-24 14:54:38 +0800

    add

--- src/main/java/com/example/wangwangmicro/client/OrderServiceClient.java
+++ src/main/java/com/example/wangwangmicro/client/OrderServiceClient.java
@@ -1,4 +1,14 @@
 package com.example.wangwangmicro.client;
 
-public class OrderServiceClient {
+import com.example.wangwangmicro.Entity.Order;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@FeignClient(name = "order-service")
+public interface OrderServiceClient {
+
+    @PostMapping("/orders")
+    String createOrder(@RequestBody Order order);
+
 }
