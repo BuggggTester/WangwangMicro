de33cd5001a0e4aad9a4cee1164a10c311193e44
xsrkt26 <xsrkt26@outlook.com>
2024-08-24 12:01:01 +0800

    add feign

--- src/main/java/com/example/wangwangmicro/service/HotelServiceImpl.java
+++ src/main/java/com/example/wangwangmicro/service/HotelServiceImpl.java
@@ -1,12 +1,22 @@
 package com.example.wangwangmicro.service;
 
+import com.example.wangwangmicro.client.HotelServiceClient;
 import com.example.wangwangmicro.common.constant.RoomType;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 
 @Service
 public class HotelServiceImpl implements HotelService {
+
+    @Autowired
+    private HotelServiceClient hotelServiceClient;
+
+    public boolean isRoomAvailable(int hotelId, String roomType, String startDate, String endDate) {
+        return hotelServiceClient.checkRoomAvailability(hotelId, roomType, startDate, endDate);
+    }
+
     @Override
     public boolean hasAbilityRoom(int hotelId, RoomType roomType, LocalDate startDate, LocalDate endDate) {
         return false;
