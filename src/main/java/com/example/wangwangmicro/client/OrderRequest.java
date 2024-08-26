b62a369849924347b90784b3f626414b11671828
xsrkt26 <xsrkt26@outlook.com>
2024-08-26 10:04:06 +0800

    add userId/payment

--- src/main/java/com/example/wangwangmicro/client/OrderRequest.java
+++ src/main/java/com/example/wangwangmicro/client/OrderRequest.java
@@ -3,13 +3,16 @@ package com.example.wangwangmicro.client;
 import com.example.wangwangmicro.common.constant.RoomType;
 import lombok.Data;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 
 @Data
 public class OrderRequest {
+    private int userId;
     private int hotelId;
     private RoomType roomType;
     private LocalDate startDate;
     private LocalDate endDate;
+    private BigDecimal payment;
 }
 
