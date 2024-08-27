036d72c951084005dc95b46a06e4db537a6f1e85
xsrkt26 <xsrkt26@outlook.com>
2024-08-27 11:48:02 +0800

    delete get/set

--- src/main/java/com/example/wangwangmicro/client/OrderRequest.java
+++ src/main/java/com/example/wangwangmicro/client/OrderRequest.java
@@ -7,16 +7,4 @@ public class OrderRequest {
     private int foodId;
     private int tripId;
     private int quantity;
-
-    public int getFoodId() { return foodId; }
-    public void setFoodId(int foodId) { this.foodId = foodId; }
-
-    public int getUserId() { return userId; }
-    public void setUserId(int userId) { this.userId = userId; }
-
-    public int getTripId() { return tripId; }
-    public void setTripId(int tripId) { this.tripId = tripId; }
-
-    public int getQuantity() { return quantity; }
-    public void setQuantity(int quantity) { this.quantity = quantity; }
 }
