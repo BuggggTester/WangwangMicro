15c162d3fcbe4722e7c0fadcb5782b128ad10fa2
xsrkt26 <xsrkt26@outlook.com>
2024-08-28 10:53:47 +0800

    11

--- src/main/java/com/example/wangwangmicro/client/TripRequest.java
+++ src/main/java/com/example/wangwangmicro/client/TripRequest.java
@@ -1,9 +1,17 @@
 package com.example.wangwangmicro.client;
 
 
+import com.example.wangwangmicro.constant.SeatType;
 import lombok.Data;
 
 @Data
 public class TripRequest {
-
+    private int tripId;
+    private int carriage;
+    private int row;
+    private char seat;
+    private int departureId;
+    private int destinationId;
+    private SeatType seatType;
+    private int passageId;
 }
